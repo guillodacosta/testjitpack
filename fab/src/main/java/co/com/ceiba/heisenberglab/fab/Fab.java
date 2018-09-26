686ebdd08c8c16ebca7b6aa2a0af4a2a1fcaefbc
Guillermo Díaz Villegas - Ceiba Software <guillermo.diaz@MacBook-Pro-de-Soporte.local>
2018-09-26 07:58:20 -0500

    js

---
+++ fab/src/main/java/co/com/ceiba/heisenberglab/fab/Fab.java
@@ -0,0 +1,30 @@
+package co.com.ceiba.heisenberglab.fab;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.support.constraint.ConstraintLayout;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class Fab extends ConstraintLayout {
+
+    public Fab(Context context) {
+        super(context);
+    }
+
+    public Fab(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Fab(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ sample/src/main/java/co/com/ceiba/heisenberglab/heisenberglab_fab/MainActivity.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.heisenberglab.heisenberglab_fab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
